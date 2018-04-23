3efa9b5e30e978282a768a94461783c85cd4637f
no <1422537078@qq.com>
2018-04-23 12:10:32 +0800

    complete function for api 14

--- src/main/java/com/chenhai/educationsystem/controller/StudentController.java
+++ src/main/java/com/chenhai/educationsystem/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.chenhai.educationsystem.controller;
 
+import com.chenhai.educationsystem.dto.StudentIdDto;
 import com.chenhai.educationsystem.exception.GlobalException;
 import com.chenhai.educationsystem.service.StudentService;
 import com.chenhai.educationsystem.vo.StudentResult;
+import com.chenhai.educationsystem.vo.SuccessResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +20,10 @@ public class StudentController {
     @PostMapping("/list")
     public StudentResult list() throws GlobalException {
         return studentService.list();
+    }
+
+    @PostMapping("/delete")
+    public SuccessResult delete(@RequestBody StudentIdDto studentIdDto) throws GlobalException {
+        return studentService.delete(studentIdDto);
     }
 }
--- src/main/java/com/chenhai/educationsystem/service/StudentService.java
+++ src/main/java/com/chenhai/educationsystem/service/StudentService.java
@@ -1,10 +1,12 @@
 package com.chenhai.educationsystem.service;
 
 import com.chenhai.educationsystem.domain.Student;
+import com.chenhai.educationsystem.dto.StudentIdDto;
 import com.chenhai.educationsystem.exception.GlobalException;
 import com.chenhai.educationsystem.message.Message;
 import com.chenhai.educationsystem.repository.StudentRepository;
 import com.chenhai.educationsystem.vo.StudentResult;
+import com.chenhai.educationsystem.vo.SuccessResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,15 @@ public class StudentService {
         try{
             List<Student> studentList = studentRepository.findAll();
             return new StudentResult(studentList);
+        } catch (Exception e){
+            throw new GlobalException(Message.ERROR);
+        }
+    }
+
+    public SuccessResult delete(StudentIdDto studentIdDto) throws GlobalException {
+        try {
+            studentRepository.deleteById(studentIdDto.getStudentId());
+            return new SuccessResult();
         } catch (Exception e){
             throw new GlobalException(Message.ERROR);
         }
